330569eb418b9e81d158c370be5a68b161758799
vasylklapatyi <54219187+vasylklapatyi@users.noreply.github.com>
2021-02-01 11:57:39 +0200

    Initial.Projects added

---
+++ CSProject/CSProject/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+using System.IO.Pipes;
+using System.Text;
+
+namespace CSProject
+{
+
+    public static class Program
+    {
+        private static NamedPipeServerStream _server;
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("C# SIDE");
+            var namedPipeServer = new NamedPipeServerStream("my-very-cool-pipe-example", PipeDirection.InOut, 1, PipeTransmissionMode.Byte);
+            var streamReader = new StreamReader(namedPipeServer);
+            namedPipeServer.WaitForConnection();
+
+            var writer = new StreamWriter(namedPipeServer);
+            writer.Write("Hello from c#");
+            writer.Write((char)0);
+            writer.Flush();
+            namedPipeServer.WaitForPipeDrain();
+
+            Console.WriteLine($"read from pipe client: {streamReader.ReadLine()}");
+            namedPipeServer.Dispose();
+        }
+    }
+}
